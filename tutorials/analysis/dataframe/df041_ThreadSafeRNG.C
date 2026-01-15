ebdd0d02ff2c70ef212d7f38184c95725a889c40
Bohdan Dudar <bohdan96@gmail.com>
2026-01-15 11:21:31 +0100

    Add example on thread-safe usage of RNGs

---
+++ tutorials/analysis/dataframe/df041_ThreadSafeRNG.C
@@ -0,0 +1,76 @@
+/// \file
+/// \ingroup tutorial_dataframe
+/// \notebook -nodraw
+/// Usage of multithreading mode with random generators.
+///
+/// This tutorial illustrates how to make a thread-safe program with thread-local random number engines.
+/// Using only one random number generator in a ROOT::EnableImplicitMT() mode is a common pitfal.
+/// This pitfal creates race conditions resulting in a distorded random distribution.
+/// This example illustrates how to define random number generators thread-safely avoid race condition.
+/// This example also illustrates the difference between wrong and correct distributions.
+///
+/// \macro_code
+/// \macro_image
+/// \macro_output
+///
+/// \date 2025
+/// \author Bohdan Dudar (JGU Mainz) and Fernando Hueso-González (IFIC)
+
+#include <iostream>
+#include <thread>
+#include <random>
+#include "TRandom3.h"
+#include "TCanvas.h"
+#include "ROOT/RDataFrame.hxx"
+
+// Canvas that should survive the running of this macro:
+TCanvas *c1;
+
+std::mt19937 global_generator( rand() );
+std::normal_distribution<double> global_gaus(0., 1.);
+
+
+double GetRNG_global(){
+    return global_gaus( global_generator );
+}
+
+double GetRNG_thread_safe(unsigned int slot){
+   static thread_local std::mt19937 generator( rand() );
+   static thread_local std::normal_distribution<double> gaus(0., 1.);
+   return gaus( generator );
+}
+
+void df041_ThreadSafeRNG(){
+    c1 = new TCanvas("c1", "c1", 1500, 500);
+    c1->Divide(3, 1);
+    
+    // 1. Single thread for reference
+    ROOT::DisableImplicitMT();
+    auto df1 = ROOT::RDataFrame(10000000).Define("x", GetRNG_global);
+    auto h1 = df1.Histo1D({"h1", "Single thread (no MT)", 1000, -4, 4}, {"x"});    
+    c1->cd(1);
+    h1->DrawClone();
+
+    
+    // 2. Enable MT and generate random variables with global generator
+    // NOTE: Causes race conditions! Don't do it in your code!
+    // More cores - more noticeable effect
+    ROOT::EnableImplicitMT(32);
+    auto df2 = ROOT::RDataFrame(10000000).Define("x", GetRNG_global);
+    auto h2 = df2.Histo1D({"h3", "Global generator (MT)", 1000, -4, 4}, {"x"});
+    c1->cd(2);
+    h2->DrawClone();
+
+    // 2. Generate random variables with several per-thread generators
+    // NOTE: Do this!
+    auto df3 = ROOT::RDataFrame(10000000).DefineSlot("x", GetRNG_thread_safe);
+    auto h3 = df3.Histo1D({"h4", "Thread-safe generators (MT)", 1000, -4, 4}, {"x"});
+    c1->cd(3);
+    h3->DrawClone();
+
+    std::cout<<std::fixed<<std::setprecision(3)<<"Final distributions        : "<<"Mean "<<" +- "<<"STD  "<<std::endl;
+    std::cout<<std::fixed<<std::setprecision(3)<<"Theoretical                : "<<"0.000"<<" +- "<<"1.000"<<std::endl;
+    std::cout<<std::fixed<<std::setprecision(3)<<"Single thread       (no MT): "<<h1->GetMean()<<" +- "<<h1->GetStdDev()<<std::endl;
+    std::cout<<std::fixed<<std::setprecision(3)<<"Race condition         (MT): "<<h2->GetMean()<<" +- "<<h2->GetStdDev()<<std::endl;
+    std::cout<<std::fixed<<std::setprecision(3)<<"Thread-safe            (MT): "<<h3->GetMean()<<" +- "<<h3->GetStdDev()<<std::endl;   
+}
